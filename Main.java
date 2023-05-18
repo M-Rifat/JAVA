5bdfc5894bc9cddae4eabd658384fadab3dc2a5f
Md. Mobinul Islam <99177014+M-Rifat@users.noreply.github.com>
2023-05-18 21:15:49 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,80 @@
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/*
+ * @author Rifat
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FastReader input = new FastReader();
+        PrintWriter output = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int t = input.nextInt();
+        for (int tc = 0; tc < t; tc++) {
+            /*............*/
+
+            
+
+
+            /*............*/
+        }
+        output.close();
+    }
+
+    //Fast Reader for input
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
